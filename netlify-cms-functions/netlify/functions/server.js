aa6ad6f71c92a255b93a700e0fc94a6c5a80228b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-06-07 15:08:19 +0900

    netlify functions にexpressの環境を追加した

--- netlify-cms-functions/netlify/functions/server.js
+++ netlify-cms-functions/netlify/functions/server.js
@@ -1,4 +1,22 @@
+const serverless = require('serverless-http'); //追加
+const express = require('express');
+
+const app = express();
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello from root!',
+  });
+});
+
+app.use('/.netlify/functions/server', router);
+
+module.exports = app; //追加
+module.exports.handler = serverless(app); //追加
+
 // For more info, check https://docs.netlify.com/functions/build-with-javascript
+/*
 module.exports.handler = async function(event, context) {
   console.log("queryStringParameters", event.queryStringParameters)
   return {
@@ -9,7 +27,7 @@ module.exports.handler = async function(event, context) {
     })
   }
 }
-
+*/
 // Now you are ready to access this API from anywhere in your Gatsby app! For example, in any event handler or lifecycle method, insert:
 // fetch("/.netlify/functions/hello")
 //    .then(response => response.json())
