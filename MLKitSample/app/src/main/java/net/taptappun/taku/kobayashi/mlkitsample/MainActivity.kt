3de71815162e7f3e85da6b7758d6c5f0eaba170d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-08-07 03:48:47 +0900

    add barcode

--- MLKitSample/app/src/main/java/net/taptappun/taku/kobayashi/mlkitsample/MainActivity.kt
+++ MLKitSample/app/src/main/java/net/taptappun/taku/kobayashi/mlkitsample/MainActivity.kt
@@ -1,8 +1,18 @@
 package net.taptappun.taku.kobayashi.mlkitsample
 
+import android.app.Activity
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseIntArray
+import android.view.Surface
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.common.Barcode
 import net.taptappun.taku.kobayashi.mlkitsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +27,51 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
+
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_AZTEC)
+            .build()
+    }
+
+    /**
+     * Get the angle by which an image must be rotated given the device's current
+     * orientation.
+     */
+    @Throws(CameraAccessException::class)
+    private fun getRotationCompensation(cameraId: String, activity: Activity, isFrontFacing: Boolean): Int {
+        val ORIENTATIONS = SparseIntArray()
+        ORIENTATIONS.append(Surface.ROTATION_0, 0)
+        ORIENTATIONS.append(Surface.ROTATION_90, 90)
+        ORIENTATIONS.append(Surface.ROTATION_180, 180)
+        ORIENTATIONS.append(Surface.ROTATION_270, 270)
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        val deviceRotation = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (this.display == null){
+                0
+            } else {
+                this.display!!.rotation
+            }
+        } else {
+            this.windowManager.defaultDisplay.rotation
+        }
+        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
+
+        // Get the device's sensor orientation.
+        val cameraManager = activity.getSystemService(CAMERA_SERVICE) as CameraManager
+        val sensorOrientation = cameraManager
+            .getCameraCharacteristics(cameraId)
+            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
+
+        if (isFrontFacing) {
+            rotationCompensation = (sensorOrientation + rotationCompensation) % 360
+        } else { // back-facing
+            rotationCompensation = (sensorOrientation - rotationCompensation + 360) % 360
+        }
+        return rotationCompensation
     }
 
     /**
