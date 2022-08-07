2300d898312bd3111c063fde53b00bcfb088f99b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-08-07 19:42:34 +0900

    mediaprojection の開始許可を聞く

--- AndroidScreenRecord/app/src/main/java/net/taptappun/taku/kobayashi/androidscreenrecord/MainActivity.kt
+++ AndroidScreenRecord/app/src/main/java/net/taptappun/taku/kobayashi/androidscreenrecord/MainActivity.kt
@@ -1,11 +1,34 @@
 package net.taptappun.taku.kobayashi.androidscreenrecord
 
+import android.Manifest
+import android.app.Activity
+import android.app.Service
+import android.content.Intent
+import android.media.projection.MediaProjectionManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaProjectionManager = getSystemService(Service.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Log.d(TAG, "Capture OK")
+            }
+        }
+        launcher.launch(mediaProjectionManager.createScreenCaptureIntent())
+    }
+
+    companion object {
+        public const val TAG = "AndroidScreenRecord"
     }
 }
