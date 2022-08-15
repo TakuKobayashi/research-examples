080adca53a121a9ea3b486e828ac9fa8fa5b05bb
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-08-15 17:51:41 +0900

    Nearbyのプロジェクトの作成

---
+++ NearbyConnectionSample/app/src/main/java/net/taptappun/taku/kobayashi/nearbyconnectionsample/MainActivity.kt
@@ -0,0 +1,34 @@
+package net.taptappun.taku.kobayashi.nearbyconnectionsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import net.taptappun.taku.kobayashi.nearbyconnectionsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'nearbyconnectionsample' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'nearbyconnectionsample' library on application startup.
+        init {
+            System.loadLibrary("nearbyconnectionsample")
+        }
+    }
+}
