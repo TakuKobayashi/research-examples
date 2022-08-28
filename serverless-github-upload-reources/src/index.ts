e0fe2437322b985d49173ef295f33b9d25569b2a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-08-29 01:19:10 +0900

    github api を使ってファイルをアップロードを試みる

--- serverless-github-upload-reources/src/index.ts
+++ serverless-github-upload-reources/src/index.ts
@@ -1,10 +1,29 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import fastify from 'fastify';
+import { Octokit } from 'octokit';
+import fs from 'fs'
+
+const octokit = new Octokit({ auth: process.env.PERSONAL_ACCESS_TOKEN });
+const owner = 'TakuKobayashi';
+const repo = 'serverless-github-upload-reources';
 
 const app = fastify();
 
 app.get('/', async (request, reply) => {
   return {hello: "world"}
+})
+
+app.get('/upload', async (request, reply) => {
+  const image = fs.readFileSync("sample.jpg", 'base64')
+  console.log(image)
+  const res = await octokit.rest.repos.createOrUpdateFileContents({
+    owner,
+    repo,
+    path: "sample.jpg",
+    message: "test message",
+    content: image,
+  })
+  return res.data
 })
 
 export const handler = awsLambdaFastify(app);
