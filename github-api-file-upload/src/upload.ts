53c33b3190ca5dfef6ae9a77919882bc280c246d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-02 01:26:05 +0900

    cliよりGithub APIを使ってファイルをアップロードするサンプルを作ってみた

--- github-api-file-upload/src/upload.ts
+++ github-api-file-upload/src/upload.ts
@@ -1,1 +1,23 @@
-console.log("hello")
+import { Octokit } from 'octokit';
+import { config } from 'dotenv';
+import * as fs from 'fs';
+
+config();
+
+const octokit = new Octokit({ auth: process.env.PERSONAL_ACCESS_TOKEN });
+const owner = 'TakuKobayashi';
+const repo = 'research-examples';
+const content = fs.readFileSync('sample.jpg');
+
+const executeUploadFunc = async () => {
+  const res = await octokit.rest.repos.createOrUpdateFileContents({
+    owner: owner,
+    repo: repo,
+    path: 'sample.jpg',
+    message: 'upload sample.jpg',
+    content: Buffer.from(content).toString('base64'),
+    branch: 'master',
+  });
+  console.log(res.data);
+};
+executeUploadFunc();
