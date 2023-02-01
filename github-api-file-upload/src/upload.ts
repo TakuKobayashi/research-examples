6bf18284b9005070ce622c4ea4d03139e6b4c89a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-02 02:20:48 +0900

    getContentを試した

--- github-api-file-upload/src/upload.ts
+++ github-api-file-upload/src/upload.ts
@@ -7,16 +7,27 @@ config();
 const octokit = new Octokit({ auth: process.env.PERSONAL_ACCESS_TOKEN });
 const owner = 'TakuKobayashi';
 const repo = 'research-examples';
-const content = fs.readFileSync('sample.jpg');
+const uploadFileName = 'sample.jpg'
+const content = fs.readFileSync(uploadFileName);
 
 const executeUploadFunc = async () => {
+  /*
+  const contentRes = await octokit.rest.repos.getContent({
+    owner: owner,
+    repo: repo,
+    path: uploadFileName,
+  });
+  */
+  // 新しくファイルが作られる場合はこのままでOK
+  // ファイルを更新するときはファイルSHAの値を指定する必要がある(上記のコメントを外す)
   const res = await octokit.rest.repos.createOrUpdateFileContents({
     owner: owner,
     repo: repo,
-    path: 'sample.jpg',
-    message: 'upload sample.jpg',
+    path: uploadFileName,
+    message: `upload ${uploadFileName}`,
     content: Buffer.from(content).toString('base64'),
     branch: 'master',
+//    sha: contentRes.data.sha,
   });
   console.log(res.data);
 };
