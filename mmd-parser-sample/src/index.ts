b169635c9a78fb09eab454657f0b35018291427c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-12 15:14:08 +0900

    名前が変わったので

--- mmd-parser-sample/src/index.ts
+++ mmd-parser-sample/src/index.ts
@@ -10,7 +10,7 @@ app.get('/', async (request, reply) => {
 });
 
 app.get('/load_pmx', async (request, reply) => {
-  const pmxResponse = await axios.get('https://github.com/TakuKobayashi/research-examples/raw/master/threejs-mmd-parser-sample/sample_model/PronamaChan.pmx', { responseType: 'arraybuffer' })
+  const pmxResponse = await axios.get('https://github.com/TakuKobayashi/research-examples/raw/master/mmd-parser-sample/sample_model/PronamaChan.pmx', { responseType: 'arraybuffer' })
   const pmxBuffer = pmxResponse.data;
   const pmxArrayBuffer =  pmxBuffer.buffer.slice(pmxBuffer.byteOffset, pmxBuffer.byteOffset + pmxBuffer.byteLength);
   const parser = new Parser();
@@ -19,7 +19,7 @@ app.get('/load_pmx', async (request, reply) => {
 });
 
 app.get('/load_vmd', async (request, reply) => {
-  const vmdResponse = await axios.get('https://github.com/TakuKobayashi/research-examples/raw/master/threejs-mmd-parser-sample/sample_model/run.vmd', { responseType: 'arraybuffer' })
+  const vmdResponse = await axios.get('https://github.com/TakuKobayashi/research-examples/raw/master/mmd-parser-sample/sample_model/run.vmd', { responseType: 'arraybuffer' })
   const vmdBuffer = vmdResponse.data;
   const vmdArrayBuffer =  vmdBuffer.buffer.slice(vmdBuffer.byteOffset, vmdBuffer.byteOffset + vmdBuffer.byteLength);
   const parser = new Parser();
