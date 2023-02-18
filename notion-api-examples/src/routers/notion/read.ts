920c859139a0d9acb5dfd77a646d3ab6a94cc9ca
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-18 17:53:27 +0900

    NotionのREADのAPIを色々と実行してみた

--- notion-api-examples/src/routers/notion/read.ts
+++ notion-api-examples/src/routers/notion/read.ts
@@ -1,5 +1,26 @@
+import { Client } from "@notionhq/client"
+
+const notion = new Client({
+  auth: process.env.NOTION_INTERNAL_INTEGRATION_TOKEN,
+})
+
 export async function notionApiRead(app, opts): Promise<void> {
   app.get('/', async (req, res) => {
     return { hello: 'notion api read' };
+  });
+
+  app.get('/users', async (req, res) => {
+    const listUsersResponse = await notion.users.list({})
+    return listUsersResponse;
+  });
+
+  app.get('/me', async (req, res) => {
+    const response = await notion.users.me({});
+    return response;
+  });
+
+  app.get('/pages', async (req, res) => {
+    const response = await notion.search({})
+    return response;
   });
 }
