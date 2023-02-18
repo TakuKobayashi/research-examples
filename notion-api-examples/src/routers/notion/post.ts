17cdec3c8c6e4a4d51cf03f95c5a33b1198017ba
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-19 01:11:39 +0900

    databaseにデータを入れるサンプルを追加した

--- notion-api-examples/src/routers/notion/post.ts
+++ notion-api-examples/src/routers/notion/post.ts
@@ -28,85 +28,36 @@ export async function notionApiPost(app, opts): Promise<void> {
     return response;
   });
 
-  app.get('/create_sample_database', async (req, res) => {
+  app.get('/add_sample_database', async (req, res) => {
     const response = await notion.databases.create({
       parent: {
         type: 'page_id',
         page_id: '9b9e71fe09f34566a5aebc3748dfb338',
       },
-      title: [
-        {
-          type: 'text',
-          text: {
-            content: 'Sample Database',
-            link: null,
-          },
-        },
-      ],
       properties: {
         text: {
           title: {},
         },
-        richtext: {
-          rich_text: {},
-        },
-        checkbox: {
-          checkbox: {},
-        },
-        selectbox: {
-          select: {
-            options: [
-              {
-                name: '🥦Vegetable',
-                color: 'green',
+      },
+    });
+    return response;
+  });
+
+  app.get('/add_data_to_sample_database', async (req, res) => {
+    const response = await notion.pages.create({
+      parent: {
+        type: 'database_id',
+        database_id: '01e15323-833f-466a-9faa-42289dafc3ff',
+      },
+      properties: {
+        text: {
+          title: [
+            {
+              text: {
+                content: 'hogehoge',
               },
-              {
-                name: '🍎Fruit',
-                color: 'red',
-              },
-              {
-                name: '💪Protein',
-                color: 'yellow',
-              },
-            ],
-          },
-        },
-        price: {
-          number: {
-            format: 'yen',
-          },
-        },
-        date: {
-          date: {},
-        },
-        multi_selectbox: {
-          type: 'multi_select',
-          multi_select: {
-            options: [
-              {
-                name: 'Duc Loi Market',
-                color: 'blue',
-              },
-              {
-                name: 'Rainbow Grocery',
-                color: 'gray',
-              },
-              {
-                name: 'Nijiya Market',
-                color: 'purple',
-              },
-              {
-                name: "Gus'''s Community Market",
-                color: 'yellow',
-              },
-            ],
-          },
-        },
-        person: {
-          people: {},
-        },
-        file: {
-          files: {},
+            },
+          ],
         },
       },
     });
