17a7f4069886b42bb620c0f8bff48deebafc4f3f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-07-13 03:35:42 +0900

    とりあえずOAuthを行う土台ができた

--- twitter-api-v2-sample/src/index.ts
+++ twitter-api-v2-sample/src/index.ts
@@ -1,5 +1,6 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import fastify from 'fastify';
+import { auth } from "twitter-api-sdk";
 
 const app = fastify();
 
@@ -8,7 +9,18 @@ app.get('/', async (request, reply) => {
 });
 
 app.get('/twitter/auth', (req, reply) => {
-  reply.redirect('/twitter/oauth/callback');
+  const protocol = process.env.IS_OFFLINE ? 'http' : 'https';
+  const currentBaseUrl = [protocol + '://' + req.hostname, req.awsLambda.event.requestContext.stage].join('/');
+  const authClient = new auth.OAuth2User({
+    client_id: process.env.TWITTER_OAUTH2_CLIENT_ID,
+    callback: currentBaseUrl + "/twitter/oauth/callback",
+    scopes: ["tweet.read", "users.read", "offline.access"],
+  });
+  const authUrl = authClient.generateAuthURL({
+    state: "randomstring",
+    code_challenge_method: "s256",
+  });
+  reply.redirect(authUrl);
 });
 
 app.get('/twitter/oauth/callback', async (req, reply) => {
