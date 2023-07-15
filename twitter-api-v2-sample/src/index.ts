75a0d9debcdb2f72b2f0e0565a988ea7795cfa5a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-07-16 00:25:59 +0900

    twitter oauth v2の実装完了

--- twitter-api-v2-sample/src/index.ts
+++ twitter-api-v2-sample/src/index.ts
@@ -8,23 +8,29 @@ app.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
 
+const authClient = new auth.OAuth2User({
+  client_id: process.env.TWITTER_OAUTH2_CLIENT_ID,
+  client_secret: process.env.TWITTER_OAUTH2_CLIENT_SECRET,
+  callback: "http://localhost:3000/dev/twitter/oauth/callback",
+  scopes: ["tweet.read", "users.read", "offline.access"],
+});
+
 app.get('/twitter/auth', (req, reply) => {
-  const protocol = process.env.IS_OFFLINE ? 'http' : 'https';
-  const currentBaseUrl = [protocol + '://' + req.hostname, req.awsLambda.event.requestContext.stage].join('/');
-  const authClient = new auth.OAuth2User({
-    client_id: process.env.TWITTER_OAUTH2_CLIENT_ID,
-    callback: currentBaseUrl + "/twitter/oauth/callback",
-    scopes: ["tweet.read", "users.read", "offline.access"],
-  });
   const authUrl = authClient.generateAuthURL({
     state: "randomstring",
     code_challenge_method: "s256",
   });
+  console.log(authUrl);
   reply.redirect(authUrl);
 });
 
 app.get('/twitter/oauth/callback', async (req, reply) => {
-  return { callback: 'token' };
+  const queryObj = req.query;
+  console.log(queryObj);
+  const token = await authClient.requestAccessToken(queryObj.code).catch((e) => {
+    console.log(e);
+  });
+  return { token: token, ...queryObj };
 });
 
 export const handler = awsLambdaFastify(app);
