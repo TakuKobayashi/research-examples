6545dfdfe7dec259214634ca26800599bd19922e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-07-20 14:56:28 +0900

    local環境でD1のDBを作成して、データ追加して、データを取得する一連の流れの確認を行なった

--- cloudflare-hono-sample/src/index.ts
+++ cloudflare-hono-sample/src/index.ts
@@ -14,4 +14,16 @@ app.get('/json', (c) => {
   return c.json({hello: "Hono JSON"})
 })
 
+app.get('/db/queryping', async (c) => {
+  try {
+    const results = await c.env.DB.prepare(
+      "SELECT * FROM User",
+    )
+    .all();
+    return c.json(results);
+  } catch (e: any) {
+    return c.json({ err: e.message }, 500);
+  }
+})
+
 export default app
