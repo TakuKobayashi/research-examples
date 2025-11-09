9753ffd85484e6ab6537d4447d618264d902e43f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-11-10 03:52:08 +0900

    opencvを使ったサンプルはwebの画像もダウンロードしてグレースケールに変換することも可能にした

--- python-opencv/scripts/to_grayscale.py
+++ python-opencv/scripts/to_grayscale.py
@@ -1,6 +1,14 @@
 import sys
+import numpy as np
 import cv2
+import requests
+from urllib.parse import urlparse
 
-im = cv2.imread(sys.argv[1])
+src = sys.argv[1]
+if urlparse(src).scheme.startswith('http'):
+  im = cv2.imdecode(np.array(bytearray(requests.get(src).content), dtype=np.uint8), -1)
+else:
+  im = cv2.imread(src)
+
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 cv2.imwrite(sys.argv[2], im_gray)
