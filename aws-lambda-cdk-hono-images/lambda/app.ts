790491f98a90b620271818e96947ec5530c80897
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-11-30 02:02:10 +0900

    web上の画像をダウンロードしてきて表示できるようにした

--- aws-lambda-cdk-hono-images/lambda/app.ts
+++ aws-lambda-cdk-hono-images/lambda/app.ts
@@ -1,7 +1,15 @@
 import { Hono } from 'hono';
+import axios from 'axios';
 
 const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!おめでとうございます！')); // 確認用
+app.get('/', (c) => c.text('Hello Hono!おめでとうございます！'));
+
+app.get('/binary', async (c) => {
+  const response = await axios.get('https://t18.pimg.jp/063/748/498/1/63748498.jpg', { responseType: 'arraybuffer' });
+  c.status(200);
+  c.header('Content-Type', 'image/jpg');
+  return c.body(response.data);
+});
 
 export default app;
